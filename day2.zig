586b856c4a0630c8a6c87723bda0ca3d474c1e77
Zach Waugh <zwaugh@gmail.com>
2021-12-02 08:19:57 -0500

    Day 2: Puzzle 1 solved

---
+++ day2.zig
@@ -0,0 +1,64 @@
+const std = @import("std");
+const mem = std.mem;
+const utils = @import("utils.zig");
+
+const DataError = error{InvalidData};
+
+const Command = struct {
+    direction: Direction,
+    distance: u32,
+
+    const Direction = enum { up, down, forward };
+};
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+    const commands = try loadData(allocator, "data/day2.txt");
+
+    var horizontal_position: u32 = 0;
+    var depth: u32 = 0;
+
+    for (commands) |command| {
+        switch (command.direction) {
+            .up => depth -= command.distance,
+            .down => depth += command.distance,
+            .forward => horizontal_position += command.distance,
+        }
+    }
+
+    const result = horizontal_position * depth;
+    std.debug.print("[Puzzle 1] horizontal position: {}, depth: {}, result: {}\n", .{ horizontal_position, depth, result });
+}
+
+fn loadData(allocator: *mem.Allocator, path: []const u8) ![]const Command {
+    const input = try std.fs.cwd().readFileAlloc(allocator, path, 1024 * 1024);
+    var iterator = mem.tokenize(u8, input, "\n");
+    var commands = std.ArrayList(Command).init(allocator);
+
+    while (iterator.next()) |value| {
+        var parts = mem.split(u8, value, " ");
+        const direction_part = parts.next() orelse return DataError.InvalidData;
+        const direction = directionFromString(direction_part) orelse return DataError.InvalidData;
+        const distance_part = parts.next() orelse return DataError.InvalidData;
+        const distance = try std.fmt.parseInt(u32, distance_part, 10);
+        const command = Command{ 
+            .direction = direction, 
+            .distance = distance
+        };
+        try commands.append(command);
+    }
+
+    return commands.items;
+}
+
+fn directionFromString(string: []const u8) ?Command.Direction {
+    if (mem.eql(u8, string, "up")) {
+        return .up;
+    } else if (mem.eql(u8, string, "down")) {
+        return .down;
+    } else if (mem.eql(u8, string, "forward")) {
+        return .forward;
+    } else {
+        return null;
+    }
+}
