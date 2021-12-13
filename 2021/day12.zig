10adc6669394228934d5495c613dc26e9a207f53
Zach Waugh <zwaugh@gmail.com>
2021-12-12 21:11:37 -0500

    Day 12, Puzzle 2

--- 2021/day12.zig
+++ 2021/day12.zig
@@ -8,7 +8,7 @@ const print = @import("std").debug.print;
 pub fn main() !void {
     const graph = try loadData("data/day12.txt");
     try puzzle1(graph);
-    try puzzle2();
+    try puzzle2(graph);
 }
 
 /// Answers
@@ -21,8 +21,14 @@ fn puzzle1(graph: []Edge) !void {
     print("[Day 12/Puzzle 1] {d} paths found\n", .{paths.len});
 }
 
-fn puzzle2() !void {
-    print("[Day 12/Puzzle 2] not implemented\n", .{});
+/// Answers
+/// - Test: 36
+/// - Input: 152480
+fn puzzle2(graph: []Edge) !void {
+    print("[Day 12/Puzzle 2] processing graph with {d} edges\n", .{graph.len});
+    var visited = std.StringHashMap(u8).init(allocator);
+    const paths = findPaths2("start", graph, &visited);
+    print("[Day 12/Puzzle 2] {d} paths found\n", .{paths.len});
 }
 
 fn findPaths(start: []const u8, graph: []Edge, visited: *std.StringHashMap(void)) [][]Edge {
@@ -57,6 +63,68 @@ fn findPaths(start: []const u8, graph: []Edge, visited: *std.StringHashMap(void)
     }
 
     return paths.toOwnedSlice();
+}
+
+fn findPaths2(start: []const u8, graph: []Edge, visited: *std.StringHashMap(u8)) [][]Edge {
+    if (isLowercase(start)) {
+        if (visited.get(start)) |count| {
+            visited.put(start, count + 1) catch unreachable;
+        } else {
+            visited.put(start, 1) catch unreachable;
+        }
+    }
+
+    const edges = findEdges(start, graph) catch unreachable;
+    var paths = std.ArrayList([]Edge).init(allocator);
+
+    for (edges) |edge| {
+        if (!isVisitable(edge.end, visited)) continue;
+
+        if (mem.eql(u8, edge.end, "end")) {
+            var path = std.ArrayList(Edge).init(allocator);
+            path.append(edge) catch unreachable;
+            paths.append(path.toOwnedSlice()) catch unreachable;
+        } else {
+            var visited_copy = visited.clone() catch unreachable;
+            const child_paths = findPaths2(edge.end, graph, &visited_copy);
+            for (child_paths) |child_path| {
+                var path = std.ArrayList(Edge).init(allocator);
+                path.append(edge) catch unreachable;
+
+                for (child_path) |child_edge| {
+                    path.append(child_edge) catch unreachable;
+                }
+
+                paths.append(path.toOwnedSlice()) catch unreachable;
+            }
+        }
+    }
+
+    return paths.toOwnedSlice();
+}
+
+fn isVisitable(cave: []const u8, visited: *std.StringHashMap(u8)) bool {
+    if (isUppercase(cave)) return true;
+    if (mem.eql(u8, cave, "start")) return false;
+
+    if (visited.get(cave)) |count| {
+        if (count >= 2) {
+            return false;
+        } else {
+            // Only allow a single small cave to be visited twice
+            var iterator = visited.iterator();
+            while (iterator.next()) |entry| {
+                if (isLowercase(entry.key_ptr.*) and entry.value_ptr.* >= 2) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    } else {
+        // Not visited yet
+        return true;
+    }
 }
 
 fn findEdges(cave: []const u8, graph: []Edge) ![]Edge {
@@ -114,4 +182,8 @@ fn loadData(path: []const u8) ![]Edge {
 fn isUppercase(string: []const u8) bool {
     if (string.len == 0) return false;
     return string[0] >= 'A' and string[0] <= 'Z';
+}
+
+fn isLowercase(string: []const u8) bool {
+    return !isUppercase(string);
 }
