b646068b2610de6f84e98fa82f8387a9d1f2a638
Zach Waugh <zwaugh@gmail.com>
2022-12-02 08:16:39 -0500

    2022: Day 2, Puzzle 2

--- 2022/Source/Day2.swift
+++ 2022/Source/Day2.swift
@@ -29,6 +29,42 @@ struct Day2 {
         }
     }
 
+    enum Outcome {
+        case win, lose, draw
+
+        init(_ string: String) {
+            switch string {
+            case "X":
+                self = .lose
+            case "Y":
+                self = .draw
+            case "Z":
+                self = .win
+            default:
+                fatalError("Invalid input")
+            }
+        }
+
+        func move(for opponent: Move) -> Move {
+            switch self {
+            case .win:
+                switch opponent {
+                case .rock: return .paper
+                case .scissors: return .rock
+                case .paper: return .scissors
+                }
+            case .lose:
+                switch opponent {
+                case .rock: return .scissors
+                case .scissors: return .paper
+                case .paper: return .rock
+                }
+            case .draw:
+                return opponent
+            }
+        }
+    }
+
     struct Game {
         let me: Move
         let opponent: Move
@@ -51,26 +87,43 @@ struct Day2 {
     /// Answer: 11386
     func puzzle1() throws {
         print("[Day 2, Puzzle 1] processing...")
-        let games = try parse()
+        let games = try parse1()
         let total = games
             .map { $0.score() }
             .reduce(0, +)
         print("[Day 2, Puzzle 1] answer: \(total)")
     }
 
+    /// Answer: 13600
     func puzzle2() throws {
-        
+        print("[Day 2, Puzzle 2] processing...")
+        let games = try parse2()
+        let total = games
+            .map { $0.score() }
+            .reduce(0, +)
+        print("[Day 2, Puzzle 2] answer: \(total)")
     }
 
-    private func parse() throws -> [Game] {
+    private func parse() throws -> [[String]] {
         try Input
             .data(for: "day2.txt")
             .components(separatedBy: "\n")
             .compactMap {
                 $0.isEmpty ? nil : $0.components(separatedBy: " ")
             }
-            .map {
-                Game(me: .init($0[1]), opponent: .init($0[0]))
-            }
+    }
+
+    private func parse1() throws -> [Game] {
+        try parse().map {
+            Game(me: .init($0[1]), opponent: .init($0[0]))
+        }
+    }
+
+    private func parse2() throws -> [Game] {
+        try parse().map { line in
+            let opponent = Move(line[0])
+            let me = Outcome(line[1]).move(for: opponent)
+            return Game(me: me, opponent: opponent)
+        }
     }
 }
