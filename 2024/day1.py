2cfa60ce06e963967350629053d8c4352fa4ec5c
Zach Waugh <zwaugh@gmail.com>
2024-12-02 08:49:51 -0500

    2024: Day 1, part 2

--- 2024/day1.py
+++ 2024/day1.py
@@ -1,9 +1,31 @@
 def main():
     part1()
+    part2()
 
 def part1():
+    list1, list2 = load_data()
+    distances = 0
+
+    for index, value in enumerate(list1):
+        value2 = list2[index]
+        distances += abs(value - value2)
+
+    print("[Day 1, part 1] Distances:", distances)
+
+def part2():
+    list1, list2 = load_data()
+    similarity_score_total = 0
+
+    for value in list1:
+        count = count_occurrences(list2, value)
+        score = value * count
+        similarity_score_total += score
+
+    print("[Day 1, part 2] Similarity score:", similarity_score_total)
+
+def load_data():
     with open('day1.txt', 'r') as file:
-        content = file.read()
+        content = file.read().strip()
 
     lines = content.split('\n')
     list1 = []
@@ -16,13 +38,15 @@ def part1():
 
     list1.sort()
     list2.sort()
-    distances = 0
+    return list1, list2
 
-    for index, value in enumerate(list1):
-        value2 = list2[index]
-        distances += abs(value - value2)
+def count_occurrences(array, target):
+    count = 0
+    for value in array:
+        if value == target:
+            count += 1
 
-    print("[Day 1, part1] Distances:", distances)
+    return count
 
 if __name__ == "__main__":
     main()
