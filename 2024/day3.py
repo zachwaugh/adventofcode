05b9b2fd29b5c37d3f10b91783b280f6a93cf5e3
Zach Waugh <zwaugh@gmail.com>
2024-12-03 08:42:39 -0500

    2024: Day 3, part 2

--- 2024/day3.py
+++ 2024/day3.py
@@ -18,7 +18,23 @@ def part1():
     print("[Day 3, part 1] result:", result)
 
 def part2():
-    print("[Day 3, part 2]")
+    input = load_data()
+    pattern = r"(do\(\)|don\'t\(\)|mul\(([0-9]+),([0-9]+)\))"
+    matches = re.findall(pattern, input)
+    result = 0
+    multiplication_enabled = True
+    
+    for match in matches:
+        if match[0] == "don't()":
+            multiplication_enabled = False
+        elif match[0] == "do()":
+            multiplication_enabled = True
+        elif match[0].startswith("mul") and multiplication_enabled:
+            factor1 = int(match[1])
+            factor2 = int(match[2])
+            result += factor1 * factor2
+
+    print("[Day 3, part 2] result:", result)
 
 def load_data():
     with open('day3.txt', 'r') as file:
