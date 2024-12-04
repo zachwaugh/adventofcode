a1237fef8a25a982d6316f6ae3a536866eb6bb81
Zach Waugh <zwaugh@gmail.com>
2024-12-04 09:13:46 -0500

    2024: Day 4, part 2

--- 2024/day4.py
+++ 2024/day4.py
@@ -57,7 +57,39 @@ def part1():
     print("[Day 4, part 1] XMAS count:", xmas_count)
 
 def part2():
-    print("[Day 4, part 2]")
+    input = load_data()
+    rows = []
+    for line in input.splitlines():
+        rows.append(list(line))
+
+    boxes = []
+    box_size = 3
+    # Form all possible 3x3 boxes in puzzle
+    for row_index in range(len(rows)):
+        # Not enough rows left to form box
+        if row_index > len(rows) - box_size:
+            break
+
+        row = rows[row_index]
+        for i in range(len(row)):
+            # Not enough columns left to form box
+            if i > len(row) - box_size:
+                break
+
+            box = [
+                rows[row_index][i:i+3],
+                rows[row_index+1][i:i+3],
+                rows[row_index+2][i:i+3]
+            ]
+            boxes.append(box)
+
+    xmas_count = 0
+
+    for box in boxes:
+        if check_box(box):
+            xmas_count += 1
+
+    print("[Day 4, part 2] X-MAS count:", xmas_count)
 
 # Check each grouping of 4 chars in array forward and reverse
 def count_array(array):
@@ -68,6 +100,21 @@ def count_array(array):
             if word == "XMAS" or word == "SAMX":
                 count += 1
     return count
+
+def check_box(box):
+    diagonal_right = [
+        box[0][0],
+        box[1][1],
+        box[2][2]
+    ]
+    diagonal_left = [
+        box[0][2],
+        box[1][1],
+        box[2][0]
+    ]
+    word_left = ''.join(diagonal_left)
+    word_right = ''.join(diagonal_right)
+    return (word_left == "MAS" or word_left == "SAM") and (word_right == "MAS" or word_right == "SAM")
 
 def load_data():
     with open('day4.txt', 'r') as file:
