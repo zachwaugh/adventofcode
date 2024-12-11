dd42be0f76a7f91ae5ecb057a6b59075ef972075
Zach Waugh <zwaugh@gmail.com>
2024-12-10 21:37:25 -0500

    2024: Day 10, part 2

--- 2024/day10.py
+++ 2024/day10.py
@@ -10,42 +10,56 @@ def part1():
 
 
 def part2():
-    print("[Day 10, part 2]")
+    grid = load_data()
+    ratings = find_all_trailheads_ratings(grid)
+    print("[Day 10, part 2] ratings:", ratings)
 
 
 def search(grid, x, y):
-    results = set()
+    results = []
     value = grid[y][x]
     next = int(value) + 1
 
     up = grid[y + 1][x] if y + 1 < len(grid) else None
     if up and up == "9" and next == 9:
-        results.add((x, y + 1))
+        results.append((x, y + 1))
     elif up and up != "." and int(up) == next:
-        results |= search(grid, x, y + 1)
+        results += search(grid, x, y + 1)
 
     down = grid[y - 1][x] if y > 0 else None
     if down and down == "9" and next == 9:
-        results.add((x, y - 1))
+        results.append((x, y - 1))
     elif down and down != "." and int(down) == next:
-        results |= search(grid, x, y - 1)
+        results += search(grid, x, y - 1)
 
     left = grid[y][x - 1] if x > 0 else None
     if left and left == "9" and next == 9:
-        results.add((x - 1, y))
+        results.append((x - 1, y))
     elif left and left != "." and int(left) == next:
-        results |= search(grid, x - 1, y)
+        results += search(grid, x - 1, y)
 
     right = grid[y][x + 1] if x + 1 < len(grid[0]) else None
     if right and right == "9" and next == 9:
-        results.add((x + 1, y))
+        results.append((x + 1, y))
     elif right and right != "." and int(right) == next:
-        results |= search(grid, x + 1, y)
+        results += search(grid, x + 1, y)
 
     return results
 
 
 def find_all_trailheads(grid):
+    score = 0
+    for y, row in enumerate(grid):
+        for x, col in enumerate(row):
+            if grid[y][x] == "0":
+                results = set(search(grid, x, y))
+                trailhead_score = len(results)
+                score += trailhead_score
+
+    return score
+
+
+def find_all_trailheads_ratings(grid):
     score = 0
     for y, row in enumerate(grid):
         for x, col in enumerate(row):
