84c851c59671161c41d9439070875f23da1faf1e
Zach Waugh <zwaugh@gmail.com>
2024-12-11 20:15:19 -0500

    2024: Day 11, part 2
    I struggled on this and got a hint on Reddit about @functools.cache and just counting the stones instead of building the array

--- 2024/day11.py
+++ 2024/day11.py
@@ -1,42 +1,62 @@
+import functools
+
+
 def main():
     part1()
     part2()
 
 
 def part1():
-    stones = load_data().split(" ")
-    blink_count = 25
-    for i in range(blink_count):
-        blink(stones)
-    print("[Day 11, part 1] stones:", len(stones))
+    stones = load_data()
+    total = 0
+    for stone in stones:
+        total += count_stones(stone, 25)
+    print("[Day 11, part 1] stones:", total)
 
 
 def part2():
-    print("[Day 11, part 2]")
+    stones = load_data()
+    total = 0
+    for stone in stones:
+        total += count_stones(stone, 75)
+    print("[Day 11, part 2] stones:", total)
 
 
-def blink(stones):
-    index = 0
-    while index < len(stones):
-        stone = stones[index]
-        if stone == "0":
-            stones[index] = "1"
-        elif len(stone) % 2 == 0:
-            digits = list(stone)
-            mid = int(len(stone) / 2)
-            # Convert to int and back to string to drop leading 0s
-            stones[index] = str(int("".join(digits[:mid])))
-            stones.insert(index + 1, str(int("".join(digits[mid:]))))
-            index += 1
+def value_for_stone(stone):
+    text = str(stone)
+    if stone == 0:
+        return (1, None)
+    elif len(text) % 2 == 0:
+        digits = list(text)
+        mid = len(digits) // 2
+        left = int("".join(digits[:mid]))
+        right = int("".join((digits[mid:])))
+        return (left, right)
+    else:
+        return (stone * 2024, None)
+
+
+@functools.cache
+def count_stones(stone, depth):
+    left, right = value_for_stone(stone)
+
+    if depth == 1:
+        if right is None:
+            return 1
         else:
-            stones[index] = str(int(stone) * 2024)
-        index += 1
+            return 2
+    else:
+        output = count_stones(left, depth - 1)
+        if right is not None:
+            output += count_stones(right, depth - 1)
+
+        return output
 
 
 def load_data():
     with open("day11.txt", "r") as file:
-        content = file.read().strip()
-    return content
+        input = file.read().strip()
+    return list(map(int, input.split(" ")))
 
 
 if __name__ == "__main__":
