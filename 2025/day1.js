16aab24ebcd0a972974918c768102e91fb9d3aa3
Zach Waugh <zwaugh@gmail.com>
2025-12-02 20:43:44 -0500

    2025: Day 1, Puzzle 2

--- 2025/day1.js
+++ 2025/day1.js
@@ -33,7 +33,37 @@ const puzzle1 = () => {
   console.log("[Day 1, Puzzle 1] Answer:", zeroCount);
 };
 
-const puzzle2 = () => {};
+const puzzle2 = () => {
+  console.log("[Day 1, Puzzle 2] Starting…");
+  const input = fs.readFileSync("day1.txt", "utf8");
+  const lines = input.trim().split("\n");
+  const rotations = lines.map((line) => [line[0], parseInt(line.slice(1))]);
+
+  let position = 50;
+  let zeroCount = 0;
+
+  for (const rotation of rotations) {
+    let [direction, distance] = rotation;
+
+    while (distance > 0) {
+      position = direction == "L" ? position - 1 : position + 1;
+
+      if (position < 0) {
+        position = 99;
+      } else if (position > 99) {
+        position = 0;
+      }
+
+      if (position === 0) {
+        zeroCount++;
+      }
+
+      distance -= 1;
+    }
+  }
+
+  console.log("[Day 1, Puzzle 2] Answer:", zeroCount);
+};
 
 puzzle1();
 puzzle2();
