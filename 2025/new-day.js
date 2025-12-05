5a1fe56dfda765d25028e0b4122d3fd496311ac1
Zach Waugh <zwaugh@gmail.com>
2025-12-05 07:56:32 -0500

    Check if file exists

--- 2025/new-day.js
+++ 2025/new-day.js
@@ -7,9 +7,15 @@ if (!process.argv[2]) {
 
 const day = parseInt(process.argv[2]);
 console.log("Creating new day.js file for:", day);
+const filename = `day${day}.js`;
+
+if (fs.existsSync(filename)) {
+  console.error("File already exists!", filename);
+  process.exit(1);
+}
 
 let template = fs.readFileSync("template.js", "utf8");
 template = template.replaceAll("{{day}}", day);
 console.log(template);
 
-fs.writeFileSync(`day${day}.js`, template);
+fs.writeFileSync(filename, template);
